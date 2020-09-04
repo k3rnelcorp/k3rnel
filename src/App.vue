83dc51198c0674234ca7734948ec25f4988131b1
clust3rr <thg.santos@outlook.com>
2020-09-04 18:39:22 -0300

    a page with a logo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/k3rnel_logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+</style>
